91ebf72550b2a97386435a67194c857819648b7f
JAmador29 <josedavidamamdorespino@gmail.com>
2026-07-06 23:25:00 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,25 @@ import { glob } from 'glob';
 
 export default defineConfig(
     {
-    appType: 'Me-portafolio',
-    base: process.env.DEPLOY_BASE_URL + '/',
-    build: {
-        rollDownOptions: {
-            input: obtenerHtmlFiles()
-        }
-    },
+        appType: 'Me-portafolio',
+        base: process.env.DEPLOY_BASE_URL || '/',
+        build: {
+            rollDownOptions: {
+                input: obtenerHtmlFiles()
+            }
+        },
 
-  plugins: [
-        purgecss({
-            content: glob.sync('./index.html').concat(glob.sync('./*.js')),
-            safelist: ['carousel-btn', 'btnleft', 'btnright', 'error', 'error-text']
-        })
-    ],
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true
+        plugins: [
+            purgecss({
+                content: glob.sync('./index.html').concat(glob.sync('./*.js')),
+                safelist: ['carousel-btn', 'btnleft', 'btnright', 'error', 'error-text']
+            })
+        ],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true
+                }
             }
         }
-    }
-});
+    });
