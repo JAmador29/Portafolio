f145515b16176020257dad1f7514b5fe191ecbe5
JAmador29 <josedavidamamdorespino@gmail.com>
2026-07-06 23:31:19 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,19 @@ import { defineConfig } from 'vite';
 import purgecss from 'vite-plugin-purgecss';
 import { glob } from 'glob';
 
-export default defineConfig(
-    {
-        appType: 'Me-portafolio',
-        base: process.env.DEPLOY_BASE_URL || '/',
-        build: {
-            rollDownOptions: {
-                input: obtenerHtmlFiles()
-            }
-        },
-
-        plugins: [
-            purgecss({
-                content: glob.sync('./index.html').concat(glob.sync('./*.js')),
-                safelist: ['carousel-btn', 'btnleft', 'btnright', 'error', 'error-text']
-            })
-        ],
-        css: {
-            preprocessorOptions: {
-                less: {
-                    javascriptEnabled: true
-                }
-            }
-        }
-    });
+export default defineConfig({
+  base: process.env.DEPLOY_BASE_URL || '/',
+  plugins: [
+    purgecss({
+      content: glob.sync('./index.html').concat(glob.sync('./src/JS/*.js')),
+      safelist: ['abierto', 'activa', 'ok', 'err', 'error']
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
+});
